b6703037d39f2b928a387432240215e768ef2ecb
gio797 <g.muchaidze@yahoo.com>
2023-08-05 12:52:54 +0400

    add queries to backend

---
+++ api/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "GIO797dev",
+  database: "books-app",
+});
+
+app.get("/", (req, res) => {
+  res.json("hello from backend!");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES(?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("book has been created");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("connected to backend!");
+});
